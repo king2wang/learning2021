191c0937fde66a754f72f9cad1a3debc9b655ad2
king2wang <18738292899@139.com>
2021-03-11 22:58:09 +0800

    first
    infrastructure

---
+++ learning2021/xaw-diary-parent/diary-service/src/main/java/com/wx/diary/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.wx.diary.controller;
+
+import com.wx.diary.pojo.Stu;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping("/sayhello")
+    public String sayHello() {
+        return "Hello,World!";
+    }
+    @RequestMapping("/ask")
+    public Map<String,Object> ask(@RequestBody String name){
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        List<Stu> stus = new ArrayList<Stu>();
+        for (int i = 0; i < 3; i++) {
+            Stu stu = new Stu("" + i, "L" + i);
+            stus.add(stu);
+        }
+        resultMap.put("lists",stus);
+        return resultMap;
+    }
+}
