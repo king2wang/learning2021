1be203ad77f84cd85495772b3b02138dc3450803
king2wang <18738292899@139.com>
2021-03-14 01:11:44 +0800

    新增log4j2.yml与view切面

--- learning2021/xaw-diary-parent/diary-service/src/main/java/com/wx/diary/controller/HelloController.java
+++ learning2021/xaw-diary-parent/diary-service/src/main/java/com/wx/diary/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.wx.diary.controller;
 
 import com.wx.diary.pojo.Stu;
+import com.wx.log.LogUtils;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -20,7 +22,7 @@ public class HelloController {
         Map<String, Object> resultMap = new HashMap<String, Object>();
         List<Stu> stus = new ArrayList<Stu>();
         for (int i = 0; i < 3; i++) {
-            Stu stu = new Stu("" + i, "L" + i);
+            Stu stu = new Stu("" + i, name + i);
             stus.add(stu);
         }
         resultMap.put("lists",stus);
